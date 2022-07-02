fbad5e28e5f37cfbb52c50745db4b0c58d5faf7d
theko2fi <72862222+theko2fi@users.noreply.github.com>
2022-07-02 08:52:38 +0100

    Update CreateSharedMailboxExchangeOnline.ps1

--- CreateSharedMailboxExchangeOnline.ps1
+++ CreateSharedMailboxExchangeOnline.ps1
@@ -1,1 +1,26 @@
 
+$UserCredential = Get-Credential 
+
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+
+Import-PSSession $Session -DisableNameChecking
+
+$langages = @("user1@domain.com",
+"user2@domain.com",
+"user3@domain.com",
+)
+
+foreach ($i in $langages) {
+    $PrimarySmtpAddress = $i
+    $CharArray =$i.Split("@")
+    $Title = (Get-Culture).TextInfo.ToTitleCase($CharArray[0])
+    $Domain = (Get-Culture).TextInfo.ToUpper($CharArray[1]).Substring(0,$CharArray[1].IndexOf(".com"))
+    $Name = "$Title ($Domain)"
+ 
+    New-Mailbox -Shared -Name "$Name" -PrimarySmtpAddress "$PrimarySmtpAddress"
+    set-mailbox "$PrimarySmtpAddress" -MessageCopyForSentAsEnabled $True
+    set-mailbox "$PrimarySmtpAddress" -MessageCopyForSendOnBehalfEnabled $True
+
+} 
+ 
+Remove-PSSession $Session 
